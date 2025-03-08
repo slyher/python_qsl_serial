5384742931c6c052ce6f2e72499fef5ea68b846a
Slyher <sylwek@drezyny.pl>
2025-03-08 13:11:42 +0100

    extracting code responsible for generating qsl via latex - little refactoring

--- qsl.py
+++ qsl.py
@@ -10,28 +10,32 @@ def copy_files(current_path) -> str:
     return current_path + "/qsl_r.tex"
 
 def create_working_directory_for_qso(qso -> dict) -> str:
+    qsl_directory = qso["CALL"].replace("/","-") + qso["TIME_ON"]
     current_path = os.path.join(dist_directory, qso["CALL"].replace("/","-") + qso["TIME_ON"])
     os.mkdir(current_path)
-    return current_path
+    return [current_path, qsl_directory]
 
 def create_email_file_for_qso(email- > str, current_path -> str):
     os.system("echo " +email+ " >> "+ current_path + "/mail")
     
-def update_qsl_content(qso -> dict) -> str:
-    return contents.replace("[CALL]",qso["CALL"].replace("/","{\slash}")).replace("[TIME]",qso["TIME_ON"][:2]+":"+qso["TIME_ON"][2:]).replace("[FREQ]",qso["FREQ"]).replace("[MODE]",qso["MODE"]).replace("[RST-RECV]",qso["RST_SENT"])
+def update_qsl_content(qso -> dict,content -> str) -> str:
+    return content.replace("[CALL]",qso["CALL"].replace("/","{\slash}")).replace("[TIME]",qso["TIME_ON"][:2]+":"+qso["TIME_ON"][2:]).replace("[FREQ]",qso["FREQ"]).replace("[MODE]",qso["MODE"]).replace("[RST-RECV]",qso["RST_SENT"])
     
 def write_qsl_for_qso(content->str, qsl_r_file_name -> str):
     f = open(qsl_r_file_name, "w")
     f.write(content)
     f.close()
-        
+
+def generate_qsl_with_latex(qsl_directory):
+    os.system("./qsl.sh " + qsl_directory)
+    
 filename="hf23wtte.log.adi"
 dist_directory = os.path.join(os.getcwd(), "dist")
 
 qsos, headers = adif_io.read_from_file(filename)
 
 for qso in qsos:
-    current_path = create_working_directory_for_qso(qso)
+    current_path, qsl_directory = create_working_directory_for_qso(qso)
     if "EMAIL" in qso:
         create_email_file_for_qso( qso["EMAIL"], current_path)
     qsl_r_file_name = copy_files(current_path)
@@ -41,5 +45,5 @@ for qso in qsos:
         content = update_qsl_content(f.read())
     f.close()
     write_qsl_for_qso(content, qsl_r_file_name)
-    os.system("./qsl.sh " + qso["CALL"]+qso["TIME_ON"])
+    generate_qsl_with_latex(qsl_directory)
     
